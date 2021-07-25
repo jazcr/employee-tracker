e6da1b92771d781758360d4cf636b92148c42f56
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-07-25 13:42:45 -0600

    completed all functions. missing manager function

---
+++ config/functions.js
@@ -0,0 +1,188 @@
+const connection = require("./connection.js");
+
+const task = {
+    addDepartment: function(deptName) {
+        return new Promise(function(resolve, reject) {
+            const queryString = `INSERT INTO departments (name) VALUES (?)`;
+            connection.query(queryString, deptName, function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                console.log("Department successfully added!");
+                return resolve();
+            });
+        });
+        
+    },
+    addRole: function(roleTitle, roleSalary, deptId) {
+        return new Promise(function(resolve, reject) {
+            const queryString = "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)";
+            connection.query(queryString, [roleTitle, roleSalary, deptId],function (err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                console.log("Role successfully added!");
+                return resolve();
+            });
+        });
+        
+    },
+    addEmployee: function(firstName, lastName, roleId, mgrId) {
+        return new Promise(function(resolve, reject) {
+            const queryString = "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+            connection.query(queryString, [firstName, lastName, roleId, mgrId], function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                console.log("Employee successfully added!");
+                return resolve();
+            });
+        });
+        
+    },
+    viewEmployees: function() {
+        return new Promise(function(resolve, reject) {
+            const queryString = 'SELECT employees.id, first_name, last_name, title, salary, name, manager_id FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id';
+            connection.query(queryString, function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                let newTable = [];
+                for (let i=0; i< result.length; i++) {
+                    let manager_name = "";
+                    if (result[i].manager_id !== null) {
+                        for (let j=0; j<result.length; j++) {
+                            if (result[j].id === result[i].manager_id) {
+                                manager_name = result[j].first_name + " " + result[j].last_name;
+                            }
+                        }
+                    } else {
+                        manager_name = "Not available";
+                    }
+                    const tableElement = {
+                        "Employee ID": result[i].id,
+                        "First Name": result[i].first_name,
+                        "Last Name": result[i].last_name,
+                        "Title": result[i].title,
+                        "Salary": result[i].salary,
+                        "Department": result[i].name,
+                        "Manager": manager_name
+                    };
+                    newTable.push(tableElement);
+                }
+                console.table(newTable);
+                return resolve();
+            });
+        });
+        
+    },
+    getEmployees: function() {
+        return new Promise(function(resolve, reject) {
+            const queryString = "SELECT * FROM employees";
+            connection.query(queryString, function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                const empArray = [];
+                for (let i=0; i<result.length; i++) {
+                    const empObj = {
+                        id: result[i].id,
+                        name: result[i].first_name + " " + result[i].last_name
+                    };
+                    empArray.push(empObj);
+                }
+                return resolve(empArray);
+            });
+        });
+    },
+    viewRoles: function() {
+        return new Promise(function(resolve, reject) {
+            const queryString = "SELECT roles.id, title, salary, name FROM roles LEFT JOIN departments ON roles.department_id = departments.id";
+            connection.query(queryString, function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                const newTable = [];
+                for (let i=0; i<result.length; i++) {
+                    const roleObj = {
+                        "ID": result[i].id,
+                        "Title": result[i].title,
+                        "Salary": result[i].salary,
+                        "Department": result[i].name
+                    };
+                    newTable.push(roleObj);
+                }
+                console.table(newTable);
+                return resolve();
+            });
+        });
+        
+    },
+    getRoles: function() {
+        return new Promise(function(resolve, reject) {
+            const queryString = "SELECT * FROM roles";
+            connection.query(queryString, function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(result);
+            });
+        });
+    },
+    viewDepartments: function() {
+        return new Promise(function(resolve, reject) {
+            const queryString = "SELECT * FROM departments";
+            connection.query(queryString, function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                console.table(result);
+                return resolve();
+            });
+        });
+        
+    },
+    getDepartments: function() {
+        return new Promise(function(resolve, reject) {
+            const queryString = "SELECT * FROM departments";
+            connection.query(queryString, function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(result);
+            });
+        });
+    },
+    updateRole: function(empId, newRole) {
+        return new Promise(function(resolve, reject) {
+            const queryString = "SELECT id FROM roles WHERE title = ?";
+            connection.query(queryString, newRole, function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                const newRoleId = result[0].id;
+                const queryString = "UPDATE employees SET ? WHERE ?";
+                connection.query(queryString,
+                    [{
+                        role_id: newRoleId
+                    },
+                    {
+                        id: empId
+                    }],
+                    function(err, result) {
+                        if (err) {
+                            return reject(err);
+                        }
+                        console.log("Employee's role successfully updated!");
+                        return resolve();
+                    });
+            });
+        });
+        
+    },
+    endConnection: function() {
+        connection.end();
+    }
+};
+
+module.exports = task;
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+
+const task = require("./config/functions.js");
 const inquirer = require("inquirer");
+
 
 function mainMenu() {
     inquirer.prompt({
@@ -15,6 +18,204 @@ function mainMenu() {
             "- ***Exit***"
         ],
         name: "choice"
+    }).then(function ({ choice }) {
+        if (choice === "-View all employees") {
+            task.viewEmployees()
+                .then(function () {
+                    console.log("\n");
+                    mainMenu();
+                });
+        } else if (choice === "-View list of departments") {
+            task.viewDepartments()
+                .then(function () {
+                    console.log("\n");
+                    mainMenu();
+                });
+        } else if (choice === "-View all employee roles") {
+            task.viewRoles()
+                .then(function () {
+                    console.log("\n");
+                    mainMenu();
+                });
+        } else if (choice === "-Add employee") {
+            addEmployeePrompt();
+        } else if (choice === "-Add department") {
+            addDepartmentPrompt();
+        } else if (choice === "-Add role") {
+            addRolePrompt();
+        } else if (choice === "-Update an employee's role") {
+            updateRolePrompt();
+        } else {
+            task.endConnection();
+        }
     });
-} 
+
+}
+
+function addEmployeePrompt() {
+    task.getEmployees()
+    .then(function(res) {
+        const managerArray = [];
+        for (let i=0; i<res.length; i++) {
+            managerArray.push(res[i].name);
+        }
+        managerArray.push("none");
+        task.getRoles()
+        .then(function(response) {
+            const roleTitleArray = [];
+            for (let i=0; i<response.length; i++) {
+                roleTitleArray.push(response[i].title);
+            }
+            inquirer.prompt([{
+                type: "input",
+                message: "Enter employee's first name: ",
+                name: "firstName"
+            },
+            {
+                type: "input",
+                message: "Enter employee's last name: ",
+                name: "lastName"
+            },
+            {
+                type: "list",
+                message: "Select employee's role: ",
+                choices: roleTitleArray,
+                name: "role"
+            },
+            {
+                type: "list",
+                message: "Select employee's manager",
+                choices: managerArray,
+                name: "manager"
+            }]).then(function({firstName, lastName, role, manager}) {
+                const roleId = response[roleTitleArray.indexOf(role)].id;
+                if (manager === "none") {
+                    task.addEmployee(firstName, lastName, roleId)
+                    .then(function() {
+                        console.log("\n");
+                        mainMenu();
+                    });
+                } else {
+                    const managerId = res[managerArray.indexOf(manager)].id;
+                    task.addEmployee(firstName, lastName, roleId, managerId)
+                    .then(function() {
+                        console.log("\n");
+                        mainMenu();
+                    });
+                }
+            });
+    });
+    });
+}
+
+// Prompts user for information needed to make new department, then calls task function to add it to the database
+function addDepartmentPrompt() {
+    task.getDepartments()
+    .then(function(response) {
+        const deptArray = [];
+        for (let i=0; i<response.length; i++) {
+            deptArray.push(response[i].name);
+        }
+        inquirer.prompt({
+            type: "input",
+            message: "Enter the name of new department you'd like to add",
+            name: "deptName"
+        }).then(function({deptName}) {
+            if (deptArray.includes(deptName)) {
+                console.log("There is already a department with that name!\n");
+                mainMenu();
+            } else {
+                task.addDepartment(deptName)
+                .then(function() {
+                    console.log("\n");
+                    mainMenu();
+                });
+            }
+        });
+    });
+}
+
+// Prompts user for information needed to make a new role, then calls ORM function to add it to the database
+function addRolePrompt() {
+    task.getRoles()
+    .then(function(roles) {
+        const roleArray = [];
+        for (let i=0; i<roles.length; i++) {
+            roleArray.push(roles[i].title);
+        }
+        task.getDepartments()
+        .then(function(deptArray) {
+            const deptNames = [];
+            for (let i=0; i<deptArray.length; i++) {
+                deptNames.push(deptArray[i].name);
+            }
+            inquirer.prompt([{
+                type: "input",
+                message: "Enter the name of the role you would like to add",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "Enter the annual salary of the new role",
+                name: "salary"
+            },
+            {
+                type: "list",
+                message: "Select the department in which the new role will work",
+                choices: deptNames,
+                name: "department"
+            }]).then(function({title, salary, department}) {
+                const deptId = deptArray[deptNames.indexOf(department)].id;
+                if (roleArray.includes(title)) {
+                    console.log("Error - that title already exists!\n");
+                    mainMenu();
+                } else {
+                    task.addRole(title, salary, deptId)
+                    .then(function() {
+                        console.log("\n");
+                        mainMenu();
+                    });
+                }
+            });
+        });
+    }); 
+}
+
+// Grabs all employees, asks user which one they want to update, asks what role the employee should have, then calls task function to update the database
+function updateRolePrompt() {
+    task.getEmployees()
+    .then(function(res) {
+        const empArray = [];
+        for (let i=0; i<res.length; i++) {
+            empArray.push(res[i].name);
+        }
+        task.getRoles()
+        .then(function(response) {
+            const roleArray = [];
+            for (let i=0; i<response.length; i++) {
+                roleArray.push(response[i].title);
+            }
+            inquirer.prompt([{
+                type: "list",
+                message: "Choose the employee whose role you'd like to update",
+                choices: empArray,
+                name: "employee"
+            },
+            {
+                type: "list",
+                message: "Select the employee's new role",
+                choices: roleArray,
+                name: "role"
+            }]).then(function({employee, role}) {
+                const empId = res[empArray.indexOf(employee)].id;
+                task.updateRole(empId, role)
+                .then(function() {
+                    console.log("\n");
+                    mainMenu();
+                })
+            })
+        })
+    })
+}
+
 mainMenu();
